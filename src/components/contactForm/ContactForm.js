8aae51dc12a9bfab2cd49c8c07c935e772f28c28
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-07-01 10:04:52 -0700

    edit JSX in form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,40 +1,39 @@
 import React from 'react'
 
 export default function ContactForm() {
+
+  const handleSubmit = (event)=> { 
+    event.preventDefault();
+}
   return (
     <>
         <h2>Let's Connect!</h2>
         <form
             name="contact v1"
             method="post"
+            onSubmit="submit"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
         >
-            <div hidden>
-                <input name="bot-field"/>
-            </div>
 
-            <input type="hidden" name="form-name" value="contact v1"/>
-            <div>
+            <input type="hidden" name="form-name" value="contact v1" />
+           
                 <label> Name <br/>
                     <input type="text" name="name"/>
                 </label>
-            </div>
-
-            <div>
+           
                 <label> Email <br/>
                     <input type="email" name="email"/>
                 </label>
-            </div>
             
-            <div>
                 <label> Add Your Message Here: <br/>
                     <textarea type="text" name="message"></textarea>
                 </label>
-            </div>
+            
             
             <button type="submit">Say it!</button>
         </form>
     </>
   )
+  
 }
